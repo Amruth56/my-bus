f18f2964219fd7bb474be9a600b661c4c3945146
Amruth <ammumandappa@gmail.com>
2024-12-20 23:24:30 +0530

    created a component and added header

---
+++ my-bus/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { HeaderComponent } from './header/header.component';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'my-bus';
+}
